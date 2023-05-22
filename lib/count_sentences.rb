4d84e9656027be8745ff5e1b180a032adbbfef77
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-22 19:52:16 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!]/)
+    sentences.reject(&:empty?).count
   end
 end
+
+sentence = "Is that the burger we are having?"
+
+puts sentence.sentence?
+puts sentence.question?
+puts sentence.exclamation?
+puts sentence.count_sentences
